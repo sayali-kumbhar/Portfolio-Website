de334cf8c435f80fec1169c5449885e02e143162
sayali-kumbhar <124874136+sayali-kumbhar@users.noreply.github.com>
2023-07-20 11:25:22 +0530

    Update app.py

--- Portfolio/app.py
+++ Portfolio/app.py
@@ -191,7 +191,7 @@ with st.container():
     st.header("Certifications")
     st.write("##")
     with text_column:
-        st.subheader("British Airways Virtual Experience Program on Forage")
+        st.write("British Airways Virtual Experience Program on Forage")
         st.write("I completed practical tasks in: Web scraping to gain company insights Predicting customer buying behaviour")
         st.markdown("[Certificate of completion](https://drive.google.com/file/d/1pdvoaviCk--ECwhKEIn0LtoS6YuaTclr/view?usp=sharing)")
 
